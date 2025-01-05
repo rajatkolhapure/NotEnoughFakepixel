211c3351270361e20dc3e56030057e9e7dba1b99
David <dabelesp2@gmail.com>
2025-01-05 14:19:36 +0100

    Three weirdos improving + Effects inventory feature

--- src/main/java/org/ginafro/notenoughfakepixel/features/skyblock/dungeons/ThreeWeirdos.java
+++ src/main/java/org/ginafro/notenoughfakepixel/features/skyblock/dungeons/ThreeWeirdos.java
@@ -1,30 +1,24 @@
 package org.ginafro.notenoughfakepixel.features.skyblock.dungeons;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.ginafro.notenoughfakepixel.Configuration;
+import org.ginafro.notenoughfakepixel.utils.RenderUtils;
 import org.ginafro.notenoughfakepixel.utils.ScoreboardUtils;
+import org.ginafro.notenoughfakepixel.variables.MobDisplayTypes;
 
+import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ThreeWeirdos {
 
-    /* VERIFIED ANSWERS
-
-    "The reward is not in my chest!",
-    "At least one of them is lying, and the reward is not in ",
-    --
-    "My chest has the reward and I'm telling the truth!",
-    --
-    --
-    --
-
-    * */
-
-    private String[] answers = {
+    private static String[] answers = {
             "The reward is not in my chest!",
             "At least one of them is lying, and the reward is not in ",
             "My chest doesn't have the reward we are all telling the truth.",
@@ -33,9 +27,13 @@ public class ThreeWeirdos {
             "Both of them are telling the truth. Also, "
     };
 
-    private Pattern threeWeirdosPattern = Pattern.compile("(§r§e\\[NPC] §r§c)(.+)(§r§f:)(.+)");
+    private static Pattern threeWeirdosPattern = Pattern.compile("(§r§e\\[NPC] §r§c)(.+)(§r§f:)(.+)");
+    private static Pattern threeWeirdosSolved = Pattern.compile("§r§a§lPUZZLE SOLVED! (?<player>.+) §r§ewasn't fooled by three weirdos!(.+)");
 
     private final Minecraft mc = Minecraft.getMinecraft();
+
+    private boolean foundResponse = false;
+    private String correctName = "";
 
     @SubscribeEvent
     public void onChatRecieve(ClientChatReceivedEvent e){
@@ -44,7 +42,8 @@ public class ThreeWeirdos {
        if (!ScoreboardUtils.currentGamemode.isSkyblock());
        if (!ScoreboardUtils.currentLocation.isDungeon()) return;
 
-       if(e.message.getFormattedText().startsWith("§c")) return;
+
+       if (e.message.getFormattedText().startsWith("§c")) return;
 
        Matcher matcher = threeWeirdosPattern.matcher(e.message.getFormattedText());
        if(matcher.find()){
@@ -54,10 +53,38 @@ public class ThreeWeirdos {
                if (message.contains(answer)) {
                    Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText("§a[Riddle Solver] §r§c" + name + "§r§f has the blessing!"));
                    e.setCanceled(true);
+                   foundResponse = true;
+                   correctName = name;
                    return;
                }
            }
+       } else if (threeWeirdosSolved.matcher(e.message.getFormattedText()).matches()){
+           foundResponse = false;
+           correctName = "";
        }
+    }
+
+    @SubscribeEvent
+    public void onRenderLast(RenderWorldLastEvent event) {
+        if (!Configuration.threeWeirdos) return;
+        if (Minecraft.getMinecraft().theWorld == null) return;
+        if (!foundResponse) return;
+        if (correctName.isEmpty()) return;
+        highlightNpc(correctName, event.partialTicks);
+    }
+
+    public void highlightNpc(String correctName, float partialTicks){
+        for (Entity entity : Minecraft.getMinecraft().theWorld.loadedEntityList) {
+            if(entity.getDisplayName().getUnformattedText().contains(correctName)){
+                RenderUtils.renderEntityHitbox(entity, partialTicks, new Color(90,255,90,198), MobDisplayTypes.NONE, false);
+            }
+        }
+    }
+
+    @SubscribeEvent()
+    public void onWorldUnload(WorldEvent.Unload event) {
+        correctName = "";
+        foundResponse = false;
     }
 
 }
