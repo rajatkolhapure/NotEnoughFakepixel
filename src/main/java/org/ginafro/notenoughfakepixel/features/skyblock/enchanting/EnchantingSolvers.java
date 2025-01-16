6165daf0d22128535f6252a00bff41400960f6f1
David <dabelesp2@gmail.com>
2025-01-16 21:32:43 +0100

    Enchanting chronomatron init(CRASH)

--- src/main/java/org/ginafro/notenoughfakepixel/features/skyblock/enchanting/EnchantingSolvers.java
+++ src/main/java/org/ginafro/notenoughfakepixel/features/skyblock/enchanting/EnchantingSolvers.java
@@ -3,6 +3,7 @@ package org.ginafro.notenoughfakepixel.features.skyblock.enchanting;
 import net.minecraft.block.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiChest;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.ContainerChest;
@@ -15,6 +16,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.ginafro.notenoughfakepixel.Configuration;
 import org.ginafro.notenoughfakepixel.utils.RenderUtils;
 import org.ginafro.notenoughfakepixel.utils.TablistParser;
+import org.ginafro.notenoughfakepixel.variables.F7ColorsDict;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -24,6 +26,9 @@ public class EnchantingSolvers {
 
     public static SolverTypes currentSolverType = SolverTypes.NONE;
     private static List<UltrasequencerSlot> ultrasequencerSlots = new ArrayList<>();
+    private static List<String> chronomatronOrder = new ArrayList<>();
+
+    private int chronomatronIndex = 0;
 
     private class UltrasequencerSlot{
         public Slot slot;
@@ -58,13 +63,14 @@ public class EnchantingSolvers {
         String chestName = TablistParser.currentOpenChestName;
         if (chestName == null || chestName.isEmpty()) return;
 
-
         if (chestName.startsWith("Chronomatron")) {
             currentSolverType = SolverTypes.CHRONOMATRON;
         } else if (chestName.startsWith("Ultrasequencer")) {
             currentSolverType = SolverTypes.ULTRASEQUENCER;
         } else if (chestName.startsWith("Super Pairs")) {
             currentSolverType = SolverTypes.SUPERPAIRS;
+        } else if (chestName.startsWith("Experiment Over")) {
+            currentSolverType = SolverTypes.NONE;
         } else {
             currentSolverType = SolverTypes.NONE;
         }
@@ -72,7 +78,6 @@ public class EnchantingSolvers {
 
     @SubscribeEvent
     public void onGuiDrawn(GuiScreenEvent.BackgroundDrawnEvent event) {
-        if (currentSolverType == SolverTypes.NONE) return;
         if (Configuration.ultraSequencerSolver && currentSolverType == SolverTypes.ULTRASEQUENCER) {
 
             if(!(event.gui instanceof GuiChest)) return;
@@ -116,6 +121,62 @@ public class EnchantingSolvers {
                 }
             }
         }
-    }
+        else if (Configuration.chronomatronSolver && currentSolverType == SolverTypes.CHRONOMATRON){
+            if(!(event.gui instanceof GuiChest)) return;
+            GuiChest chest = (GuiChest) event.gui;
+            Container container = chest.inventorySlots;
 
+            if(!(container instanceof ContainerChest)) return;
+            String title = ((ContainerChest) container).getLowerChestInventory().getDisplayName().getUnformattedText();
+            if (!title.startsWith("Chronomatron (")) return;
+            ContainerChest containerChest = (ContainerChest) container;
+            // Check if its in remember state
+            IInventory lower = ((ContainerChest) container).getLowerChestInventory();
+            ItemStack timerStack = lower.getStackInSlot(lower.getSizeInventory() - 5);
+            if (timerStack == null) return;
+            boolean isClock = timerStack.getItem() == Items.clock;
+
+            // if is not clock, then remember the items
+            if (!isClock) {
+                // getting item in slot 4
+                ItemStack itemInSlot = containerChest.inventorySlots.get(4).getStack();
+                // checking its stack quantity
+                if (itemInSlot == null) return;
+                int stackSize = itemInSlot.stackSize;
+                boolean found = false;
+                for (Slot slot : containerChest.inventorySlots){
+
+                    ItemStack item = slot.getStack();
+                    if (item == null) continue;
+                    if (Block.getBlockFromItem(item.getItem()) != Blocks.stained_hardened_clay) {
+                        found = false;
+                        continue;
+                    }
+                    if (chronomatronOrder.size() >= stackSize) continue;
+                    found = true;
+                    while (chronomatronOrder.size() < stackSize){
+                        chronomatronOrder.add(null);
+                    }
+
+                    chronomatronOrder.set(chronomatronIndex, F7ColorsDict.getColorFromMain(item.getMetadata()).toString());
+
+                }
+                if (!found) {
+                    chronomatronIndex++;
+                }
+
+            } else {
+                System.out.println(chronomatronOrder);
+                chronomatronIndex = 0;
+            }
+        }
+        else if (currentSolverType == SolverTypes.NONE){
+            if (chronomatronOrder.isEmpty() && ultrasequencerSlots.isEmpty()) return;
+
+            System.out.println("Deleting data");
+            chronomatronIndex = 0;
+            ultrasequencerSlots = new ArrayList<>();
+            chronomatronOrder = new ArrayList<>();
+        }
+    }
 }
