3560b64af3fa72b809429068e1f7e88b96e1b538
rajatkolhapure <rajatkolhapure@gmail.com>
2025-03-21 03:09:09 +0530

    Fairy soul command

--- src/main/java/org/ginafro/notenoughfakepixel/commands/Waypoint.java
+++ src/main/java/org/ginafro/notenoughfakepixel/commands/Waypoint.java
@@ -7,16 +7,31 @@ import net.minecraft.command.ICommandSender;
 public class Waypoint extends CommandBase {
     @Override
     public String getCommandName() {
-        return "";
+        return "fairysouls";
     }
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "";
+        return "/fairysouls <show/hide>";
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        if (args.length != 1) {
+            throw new WrongUsageException(getCommandUsage(sender));
+        }
 
+        String action = args[0].toLowerCase();
+        if (!action.equals("show") && !action.equals("hide")) {
+            throw new WrongUsageException(getCommandUsage(sender));
+        }
+
+        boolean show = action.equals("show");
+        // Toggle fairy soul waypoints visibility
+        // Implementation will depend on your waypoint system
+        sender.addChatMessage(new ChatComponentText(
+            EnumChatFormatting.GREEN + "Fairy Soul waypoints " + 
+            (show ? "enabled" : "disabled")
+        ));
     }
 }
